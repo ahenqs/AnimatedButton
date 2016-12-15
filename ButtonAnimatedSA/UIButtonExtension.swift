88740ab07ff979b1718d39a7e02e17c0c8084635
DA SILVA André Henrique <andre.henrique@consultant.volvo.com>
2016-12-15 12:48:01 +0100

    Add animation to button

---
+++ ButtonAnimatedSA/UIButtonExtension.swift
@@ -0,0 +1,9 @@
+//
+//  UIButtonExtension.swift
+//  ButtonAnimatedSA
+//
+//  Created by DA SILVA Andre Henrique on 15/12/2016.
+//  Copyright © 2016 a h e n q s. All rights reserved.
+//
+
+import Foundation
